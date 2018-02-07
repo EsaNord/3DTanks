23e6091aea7e64024b3780efa3027c8b890051b1
Esa Nord <esa.nord@cs.tamk.fi>
2018-02-07 17:51:55 +0200

    Assignment 1: camera follow
    Work in progress.

--- 3DTanks/Assets/Code/CameraFollow.cs
+++ 3DTanks/Assets/Code/CameraFollow.cs
@@ -10,19 +10,26 @@ namespace Tanks3D
         private float m_fDistance;
         private Transform m_tTarget;
 
+        // TODO have camera rotate with player.
+
         public void SetAngle(float angle)
         {
-            
+            m_fAngle = angle;
+
+            transform.eulerAngles = new Vector3(angle, 0, 0);
         } 
 
         public void SetDistance(float distance)
         {
-            
+            float x = distance * Mathf.Sin(m_fAngle);
+            float z = distance * Mathf.Cos(m_fAngle);
+
+            transform.position += new Vector3(0, x, -z);
         }
 
         public void SetTarget(Transform targetTransform)
         {
-            
+            transform.position = targetTransform.position;
         }         
     }
 }
